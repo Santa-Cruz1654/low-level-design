f6e9c8c272f8490be8e8f7f8c5ba7a83edda57e9
Krishna <your@email.com>
2026-08-15 19:25:32 +0530

    Remove online food ordering system

--- 02-online-food-ordering-system/strategies/credit_card_payment_strategy.py
+++
@@ -1,9 +0,0 @@
-from strategies.payment_strategy import PaymentStrategy
-
-
-class CreditCardPaymentStrategy(PaymentStrategy):
-    def __init__(self, card_number: str):
-        self._card_number = card_number
-
-    def pay(self, amount: float) -> None:
-        print(f"Paid ₹{amount} using Credit Card ({self._card_number})")
